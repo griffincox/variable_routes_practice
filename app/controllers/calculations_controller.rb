7c2d63a9f85d5489c8d2a78442c2d493925bb65d
Griffin Cox <griffincox@users.noreply.github.com>
2017-01-29 17:11:49 -0600

    implemented font awesome and bootstrap; working on styling

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,4 +2,11 @@ class CalculationsController < ApplicationController
   def instructions
     render("instructions.html.erb")
   end
+
+  def square
+    @the_number = params[:the_number]
+    @the_number = @the_number.to_i * @the_number.to_i
+    render("square.html.erb")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+  get("/square/:the_number", { :controller => "calculations", :action => "square"})
 end
